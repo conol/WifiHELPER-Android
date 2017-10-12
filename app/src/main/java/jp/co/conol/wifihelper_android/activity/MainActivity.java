feae83eeb7364e2ade0cdecf106707ef39ea3c08
itouuuuuuuuu <mfito.13@gmail.com>
2017-10-12 19:09:07 +0900

    アプリ未起動でもnfcを読み込めるようにした

--- app/src/main/java/jp/co/conol/wifihelper_android/activity/MainActivity.java
+++ app/src/main/java/jp/co/conol/wifihelper_android/activity/MainActivity.java
@@ -81,61 +81,66 @@ public class MainActivity extends AppCompatActivity
         if(WifiConnector.isEnable(getApplicationContext()))
             isWifiEnable = true;
 
-        // アプリ未起動でNFCタグが呼ばれた場合
+        // アプリ未起動でNFCタグが呼ばれた場合も読み込み
         if (getIntent() != null && NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-            Toast.makeText(this, "未起動時に読み込まれました", Toast.LENGTH_LONG).show();
+            startScan(false);
+            scanNfc(getIntent());
         }
     }
 
     @Override
     protected void onNewIntent(Intent intent) {
         if(isScanning) {
-            CNFCReaderTag tag = null;
-            Wifi wifi = null;
+            scanNfc(intent);
+        }
+    }
 
-            try {
-                tag = mCoronaNfc.getReadTagFromIntent(intent);
-            } catch (CNFCReaderException e) {
-                Log.d("CNFCReader", e.toString());
-                return;
-            }
+    private void scanNfc(Intent intent) {
+        CNFCReaderTag tag = null;
+        Wifi wifi = null;
 
-            if (tag != null) {
-                String chipId = tag.getChipIdString();
-                String serviceId = tag.getServiceIdString();
+        try {
+            tag = mCoronaNfc.getReadTagFromIntent(intent);
+        } catch (CNFCReaderException e) {
+            Log.d("CNFCReader", e.toString());
+            return;
+        }
+
+        if (tag != null) {
+            String chipId = tag.getChipIdString();
+            String serviceId = tag.getServiceIdString();
 //                Toast.makeText(this, "deviceId=" + chipId + "\njson=" + serviceId, Toast.LENGTH_LONG).show();
 
-                try {
-                    wifi = WifiHelper.parseJsonToObj(serviceId);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            try {
+                wifi = WifiHelper.parseJsonToObj(serviceId);
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
+        }
 
-            final Wifi readWifi = wifi;
+        final Wifi readWifi = wifi;
 
-            // 接続期限日時の算出
-            final Calendar expirationDay  = Calendar.getInstance();
-            expirationDay.setTime(new Date(System.currentTimeMillis()));
-            expirationDay.add(Calendar.DATE, wifi.getDays());
+        // 接続期限日時の算出
+        final Calendar expirationDay  = Calendar.getInstance();
+        expirationDay.setTime(new Date(System.currentTimeMillis()));
+        expirationDay.add(Calendar.DATE, wifi.getDays());
 
-            // nfc設定の確認
-            if(!WifiConnector.isEnable(getApplicationContext())) {
-                new AlertDialog.Builder(this)
-                        .setTitle("Wi-Fi設定")
-                        .setMessage("Wi-Fiがオフになっています\nWi-Fiをオンにしてもよろしいですか？")
-                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                WifiConnector.setEnable(MainActivity.this, true);
-                                connectWifi(readWifi, expirationDay);
-                            }
-                        })
-                        .setNegativeButton(getString(R.string.cancel), null)
-                        .show();
-            } else {
-                connectWifi(wifi, expirationDay);
-            }
+        // nfc設定の確認
+        if(!WifiConnector.isEnable(getApplicationContext())) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Wi-Fi設定")
+                    .setMessage("Wi-Fiがオフになっています\nWi-Fiをオンにしてもよろしいですか？")
+                    .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            WifiConnector.setEnable(MainActivity.this, true);
+                            connectWifi(readWifi, expirationDay);
+                        }
+                    })
+                    .setNegativeButton(getString(R.string.cancel), null)
+                    .show();
+        } else {
+            connectWifi(wifi, expirationDay);
         }
     }
 
@@ -159,34 +164,40 @@ public class MainActivity extends AppCompatActivity
         super.onPause();
         mCoronaNfc.disableForegroundDispatch(MainActivity.this);
 
-        // Wifi監視用のレシーバーを解除
+        // レシーバーを解除
         unregisterReceiver(mWifiConnectionBroadcastReceiver);
+        unregisterReceiver(mWifiStateBroadcastReceiver);
     }
 
     public void onStartScanButtonClicked(View view) {
         if(!isScanning) {
-
-            // nfc読み込み待機
-            mCoronaNfc.enableForegroundDispatch(MainActivity.this);
-            isScanning = true;
-
-            // 読み込み画面を表示
-            mScanDialogConstraintLayout.setVisibility(View.VISIBLE);
-            mScanConstraintLayout.setVisibility(View.VISIBLE);
-            mScanDialogConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_bottom));
-            mScanConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in_slowly));
+            startScan(true);
         }
+    }
+
+    private void startScan(boolean enableForegroundDispatch) {
+        // nfc読み込み待機
+        if(enableForegroundDispatch)    // enableForegroundDispatchの実行有無を判定
+            mCoronaNfc.enableForegroundDispatch(MainActivity.this);
+        isScanning = true;
+
+        // 読み込み画面を表示
+        mScanDialogConstraintLayout.setVisibility(View.VISIBLE);
+        mScanConstraintLayout.setVisibility(View.VISIBLE);
+        mScanDialogConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_from_bottom));
+        mScanConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_in_slowly));
     }
 
     public void onCancelScanButtonClicked(View view) {
         if(isScanning) {
-            cancelScan();
+            cancelScan(true);
         }
     }
 
-    private void cancelScan() {
+    private void cancelScan(boolean disableForegroundDispatch) {
         // nfc読み込み待機を解除
-        mCoronaNfc.disableForegroundDispatch(MainActivity.this);
+        if(disableForegroundDispatch)    // enableForegroundDispatchの実行有無を判定
+            mCoronaNfc.disableForegroundDispatch(MainActivity.this);
         isScanning = false;
 
         // 読み込み画面を非表示
@@ -229,7 +240,7 @@ public class MainActivity extends AppCompatActivity
 
             // 読み込み中に戻るタップでスキャン中止
             if(isScanning) {
-                cancelScan();
+                cancelScan(true);
             } else {
                 finish();
             }
@@ -257,8 +268,6 @@ public class MainActivity extends AppCompatActivity
             // 読み込み画面を非表示
             mScanConstraintLayout.setVisibility(View.GONE);
             mScanConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.fade_out_slowly));
-            mScanDialogConstraintLayout.setVisibility(View.GONE);
-            mScanDialogConstraintLayout.setAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_to_bottom));
 
             // 表示メッセージの作成
             String dialogMessage;
