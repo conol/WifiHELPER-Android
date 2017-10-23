3172d80bdfd314169848526f97f93caba7c53cd3
itouuuuuuuuu <mfito.13@gmail.com>
2017-10-23 19:18:20 +0900

    お問い合わせページへのリンク実装

--- app/src/main/java/jp/co/conol/wifihelper_android/activity/AboutAppActivity.java
+++ app/src/main/java/jp/co/conol/wifihelper_android/activity/AboutAppActivity.java
@@ -1,7 +1,10 @@
 package jp.co.conol.wifihelper_android.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import jp.co.conol.wifihelper_android.R;
 
@@ -11,5 +14,11 @@ public class AboutAppActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_app);
+    }
+
+    public void onAboutAppContactButtonClicked(View view) {
+        Uri uri = Uri.parse("https://conol.co.jp/contact/form");
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
     }
 }
