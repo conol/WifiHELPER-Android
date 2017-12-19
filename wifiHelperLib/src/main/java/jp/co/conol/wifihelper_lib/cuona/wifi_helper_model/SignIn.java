1a857df673ef2e2f5f70572fa4c3ddfec9e2b311
m.ito <m.ito@conol.jp>
2017-12-19 20:48:21 +0900

    networkId判定部分修正

---
+++ wifiHelperLib/src/main/java/jp/co/conol/wifihelper_lib/cuona/wifi_helper_model/SignIn.java
@@ -0,0 +1,16 @@
+package jp.co.conol.wifihelper_lib.cuona.wifi_helper_model;
+
+/**
+ * Created by m_ito on 2017/11/10.
+ */
+
+public class SignIn {
+
+    private String email = null;
+    private String password = null;
+
+    public SignIn(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
---
+++ wifiHelperLib/src/main/java/jp/co/conol/wifihelper_lib/cuona/wifi_helper_receiver/WifiExpiredBroadcastReceiver.java
@@ -0,0 +1,31 @@
+package jp.co.conol.wifihelper_lib.cuona.wifi_helper_receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import jp.co.conol.wifihelper_lib.cuona.WifiHelper;
+
+/**
+ * Created by Masafumi_Ito on 2017/10/06.
+ */
+
+public class WifiExpiredBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+
+        // ssidを取得
+        SharedPreferences pref = context.getSharedPreferences("wifiHelper", Context.MODE_PRIVATE);
+        String ssid = pref.getString("ssid", null);
+
+        // wifiを解除
+        WifiHelper.deleteAccessPoint(context, ssid);
+        WifiHelper.tryDisconnect(context); // removeWifiSetting後に実行
+
+        // メッセージを表示
+        Toast.makeText(context, "Wi-Fi Helperで設定したWi-Fiの有効期限が切れました", Toast.LENGTH_LONG).show();
+    }
+}
